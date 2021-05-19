f4a9f61d4b93b3984547c418e0d04e02eb2b7c1e
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-05-19 11:57:02 -0400

    again

--- w1d3/day2.js
+++ w1d3/day2.js
@@ -1,1 +1,10 @@
 "use strict";
+
+
+git status
+git add . (to stage files)
+git commit -m <file>
+git rm ...
+git log
+git diff (to see the changes)
+git commit --amend -m "updated message"  -(to commit and update message)
