5045d96cf95e9398a074a5ab2c815ba0cda06d7e
mintesGeb <73610057+mintesGeb@users.noreply.github.com>
2021-05-25 20:18:36 -0400

    update

---
+++ w2d1/app.js
@@ -0,0 +1,4 @@
+import { Person, Employee } from "./module.js";
+
+let obj1 = new Person();
+let obj2 = new Employee();
---
+++ w2d1/module.js
@@ -0,0 +1,10 @@
+export class Person {
+  constructor() {
+    console.log("a");
+  }
+}
+export class Employee {
+  constructor() {
+    console.log("b");
+  }
+}
